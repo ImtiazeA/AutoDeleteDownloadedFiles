c504ee344ecae2b23736bdf7f43ef5853b306d18
Imtiaze A <imtiaze_a@yahoo.com>
2019-01-06 18:30:32 +0600

    Update Read Only Permission and Can Delete Files

--- src/main/java/com/it/java/AccessFiles.java
+++ src/main/java/com/it/java/AccessFiles.java
@@ -3,70 +3,59 @@ package com.it.java;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.DosFileAttributeView;
 import java.nio.file.attribute.DosFileAttributes;
-import java.nio.file.attribute.FileTime;
 
-public class AccessFiles extends SimpleFileVisitor<Path> {
+public class AccessFiles implements FileVisitor<Path> {
 
-
-    @Override
     public FileVisitResult preVisitDirectory(Path pathToCurrentFolder, BasicFileAttributes attrs) throws IOException {
-
-        deleteFolder(pathToCurrentFolder);
-
+        delete(pathToCurrentFolder);
         return FileVisitResult.CONTINUE;
     }
 
-    @Override
     public FileVisitResult visitFile(Path pathToFile, BasicFileAttributes attrs) throws IOException {
-
         if (attrs.isRegularFile()) {
-
-            FileTime lastAccessTimeOfCurrentFile = attrs.lastAccessTime();
-            long timeDifference = System.currentTimeMillis() - lastAccessTimeOfCurrentFile.toMillis();
-
+            long lastAccessTime = attrs.lastAccessTime().toMillis();
+            long timeDifference = System.currentTimeMillis() - lastAccessTime;
             long monthInMillis = 24 * 60 * 60 * 1000;
 
             if (timeDifference > 2 * monthInMillis) {
-
-                DosFileAttributes dosFileAttributes = Files.readAttributes(pathToFile, DosFileAttributes.class);
-
-                if (!dosFileAttributes.isReadOnly()) {
-                    Files.deleteIfExists(pathToFile);
-                } else {
-                    System.out.println("Could Not Delete: " + pathToFile.getRoot() + pathToFile.subpath(0, pathToFile.getNameCount()));
-                }
-
+                delete(pathToFile);
             }
-
         }
         return FileVisitResult.CONTINUE;
     }
 
-    @Override
     public FileVisitResult postVisitDirectory(Path pathToCurrentFolder, IOException exc) throws IOException {
-
-        deleteFolder(pathToCurrentFolder);
-
+        delete(pathToCurrentFolder);
         return FileVisitResult.CONTINUE;
     }
 
-    @Override
     public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
         System.out.println(exc.getMessage());
         return FileVisitResult.CONTINUE;
     }
 
-    private void deleteFolder(Path pathToCurrentFolder) throws IOException {
-        if (Files.exists(pathToCurrentFolder)) {
-            DirectoryStream<Path> contentsOfCurrentDirectory = Files.newDirectoryStream(pathToCurrentFolder);
+    private void delete(Path pathToFile) throws IOException {
+        if (Files.exists(pathToFile)) {
+            DosFileAttributes dosFileAttributes = Files.readAttributes(pathToFile, DosFileAttributes.class);
 
-            if (!contentsOfCurrentDirectory.iterator().hasNext()) {
-                Files.deleteIfExists(pathToCurrentFolder);
+            if (dosFileAttributes.isReadOnly()) {
+                DosFileAttributeView dosFileAttributeView = Files.getFileAttributeView(pathToFile, DosFileAttributeView.class);
+                dosFileAttributeView.setReadOnly(false);
+            }
+
+            if (Files.isRegularFile(pathToFile)) {
+                Files.deleteIfExists(pathToFile);
+            } else if (Files.isDirectory(pathToFile)) {
+                DirectoryStream<Path> contentsOfCurrentDirectory = Files.newDirectoryStream(pathToFile);
+                if (!contentsOfCurrentDirectory.iterator().hasNext()) {
+                    Files.deleteIfExists(pathToFile);
+                }
             }
         }
     }
